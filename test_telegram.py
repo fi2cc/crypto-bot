4fa82fab81a95fcdf5fa2eac0b325c1f608a5545
fi2cc <dfitoussi@gmail.com>
2025-06-03 19:10:22 +0100

    Update test_telegram.py

--- test_telegram.py
+++ test_telegram.py
@@ -1,14 +1,1 @@
-import os
-import requests
 
-TELEGRAM_BOT_KEY = os.getenv('7957867294:AAEIHtqQjFmU2PPmbpxkw9qPCQaX0hdrgm0')
-TELEGRAM_CHAT_ID = os.getenv('1581904774')
-
-def send_test_message():
-    url = f"https://api.telegram.org/bot{TELEGRAM_BOT_KEY}/sendMessage"
-    params = {"chat_id": TELEGRAM_CHAT_ID, "text": "✅ Test Telegram message successful!"}
-    response = requests.post(url, params=params)
-    print("Telegram response:", response.json())
-
-if __name__ == "__main__":
-    send_test_message()
